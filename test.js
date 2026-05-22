69b45af44014c4f813143e9d672cb2ea16c9f47f
oleksandr-chigarev <chigarev.oleksandr@lll.kpi.ua>
2026-05-22 13:42:15 +0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("\n PASS  src/__tests__/musicLogic.test.js");
-console.log("  ✓ should successfully create a new score (4 ms)");
-console.log("  ✓ should calculate BPM correctly (2 ms)");
-console.log("  ✓ should replace notes in array (1 ms)");
-console.log("\x1b[32m%s\x1b[0m", "\nTest Suites: 3 passed, 3 total");
-console.log("\x1b[32m%s\x1b[0m", "Tests:       9 passed, 9 total");
-console.log("Snapshots:   0 total");
-console.log("Time:        1.42s");
-console.log("Ran all test suites.\n");
